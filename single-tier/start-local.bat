15aff95b35dcee9df54ef8ada9433fb480e83eb7
Morten Kromberg <mkrom@dyalog.com>
2022-10-05 08:26:41 +0200

    Fix issues starting single-tier application

--- single-tier/start-local.bat
+++ single-tier/start-local.bat
@@ -1,5 +1,5 @@
 docker context use default
-SET DATA=phonebook-data:/phonebook
+SET DATA=%~dp0\phonebook-data:/phonebook
 SET RIDE=RIDE_INIT=HTTP:*:8088
 SET JT=DYALOG_JARVIS_THREAD=DEBUG
 docker run -v %DATA% -p 8080:8080 -p 8088:8088 -e %RIDE% -e %JT% phonebook
